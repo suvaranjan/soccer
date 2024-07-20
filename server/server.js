22fda1364d4ba87301ecc15bf2ffa37cef1d2d36
suvaranjan <suvaranjan01@gmail.com>
2024-07-20 19:30:59 +0530

    uploads folder move to inside server dir

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
 // Routes
 app.get('/', (req, res) => {
